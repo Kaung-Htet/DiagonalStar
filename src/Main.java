04f72ea4534973f069d794fa4b2abcd150522220
Kaung-Htet <kaunghtetnyilay@gmail.com>
2020-07-02 22:11:00 +0700

    Add printSquareStar method

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String[] args){
+        DiagonalStar.printSquareStar(8);
+        DiagonalStar.printSquareStar(9);
+        DiagonalStar.printSquareStar(34);
+    }
+}
